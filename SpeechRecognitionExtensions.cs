42c5958cf7cdabef6f5126ecb5c2db80fda867bd
jeremylindsayni <jeremy.lindsay@outlook.com>
2016-07-03 14:13:40 +0100

    Initial commit.

---
+++ SpeechRecognitionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Media.SpeechRecognition;
+
+namespace Magellanic.Speech.Recognition
+{
+    public static class SpeechRecognitionExtensions
+    {
+        public static async Task Dispose(this SpeechRecognitionManager speechRecognitionManager)
+        {
+            await speechRecognitionManager.SpeechRecognizer.ContinuousRecognitionSession.StopAsync();
+
+            speechRecognitionManager.SpeechRecognizer.Dispose();
+
+            speechRecognitionManager.SpeechRecognizer = null;
+        }
+
+        public static string GetInterpretation(this SpeechRecognitionSemanticInterpretation interpretation, string key)
+        {
+            if (interpretation.Properties.ContainsKey(key))
+            {
+                return interpretation.Properties[key][0];
+            }
+
+            return null;
+        }
+
+        public static bool IsRecognisedWithHighConfidence(this SpeechRecognitionResult result)
+        {
+            return result.Confidence == SpeechRecognitionConfidence.High;
+        }
+    }
+}
---
+++ SpeechRecognitionManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Media.SpeechRecognition;
+
+namespace Magellanic.Speech.Recognition
+{
+    public class SpeechRecognitionManager
+    {
+        public SpeechRecognitionManager(string grammarFile)
+        {
+            this.SpeechRecognizer = new SpeechRecognizer();
+
+            this.GrammarFile = string.Format(grammarFile);
+        }
+
+        public SpeechRecognizer SpeechRecognizer { get; set; }
+
+        private string GrammarFile { get; set; }
+
+        public async Task CompileGrammar()
+        {
+            var grammarContentFile = await Package.Current.InstalledLocation.GetFileAsync(this.GrammarFile);
+
+            var grammarConstraint = new SpeechRecognitionGrammarFileConstraint(grammarContentFile);
+
+            this.SpeechRecognizer.Constraints.Add(grammarConstraint);
+
+            var compilationResult = await this.SpeechRecognizer.CompileConstraintsAsync();
+
+            if (compilationResult.Status == SpeechRecognitionResultStatus.Success)
+            {
+                await this.SpeechRecognizer.ContinuousRecognitionSession.StartAsync();
+                return;
+            }
+
+            throw new Exception($"Grammar file was not compiled successfully: {compilationResult.Status}");
+        }
+    }
+}
